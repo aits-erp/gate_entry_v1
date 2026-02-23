17f0149298e24badc2c06f8ce5f142394f0c2302
aits-erp <backup@aitsind.com>
2026-02-23 15:56:17 +0530

    Update api.py
    this also po re

--- gate_entry_app/api.py
+++ gate_entry_app/api.py
@@ -1,12 +1,36 @@
 import frappe
 
 @frappe.whitelist()
-def get_po_items(po):
+def get_multiple_po_items(po_list):
 
-    items = frappe.get_all(
-        "Purchase Order Item",
-        filters={"parent": po},
-        fields=["item_code","item_name","qty","uom"]
-    )
+    if isinstance(po_list, str):
+        po_list = frappe.parse_json(po_list)
+
+    items = []
+
+    for po in po_list:
+        po_items = frappe.get_all(
+            "Purchase Order Item",
+            filters={"parent": po},
+            fields=["item_code","item_name","qty","uom"]
+        )
+        items.extend(po_items)
+
+    return items
+
+    if isinstance(po_list, str):
+        po_list = frappe.parse_json(po_list)
+
+    items = []
+
+    for po in po_list:
+
+        po_items = frappe.get_all(
+            "Purchase Order Item",
+            filters={"parent": po},
+            fields=["item_code","item_name","qty","uom"]
+        )
+
+        items.extend(po_items)
 
     return items
