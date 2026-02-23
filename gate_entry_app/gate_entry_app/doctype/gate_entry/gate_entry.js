68bd0d046d4cbf96d524bc0c18eb34fad4c9c7e8
aits-erp <backup@aitsind.com>
2026-02-23 15:54:57 +0530

    Update gate_entry.js
    multi po selection

--- gate_entry_app/gate_entry_app/doctype/gate_entry/gate_entry.js
+++ gate_entry_app/gate_entry_app/doctype/gate_entry/gate_entry.js
@@ -8,37 +8,65 @@
 // });
 
 
-
 frappe.ui.form.on("Gate Entry", {
+    refresh(frm) {
 
-    purchase_order(frm) {
+        frm.add_custom_button("Get Items", () => {
 
-        if(!frm.doc.purchase_order) return;
+            // ✅ dialog reference store karo
+            let d = new frappe.ui.form.MultiSelectDialog({
+                doctype: "Purchase Order",
+                target: frm,
 
-        frappe.call({
-            method: "gate_entry_app.api.get_po_items",
-            args: { po: frm.doc.purchase_order },
-            callback: function(r){
+                setters: {
+                    supplier: null
+                },
 
-                if(r.message){
+                add_filters_group: 1,
+                date_field: "transaction_date",
+                columns: ["name", "supplier", "transaction_date"],
 
-                    frm.clear_table("items");
+                get_query() {
+                    return {
+                        filters: {
+                            docstatus: 1
+                        }
+                    };
+                },
 
-                    r.message.forEach(row => {
+                action(selections) {
 
-                        let child = frm.add_child("items");
+                    if (!selections.length) {
+                        frappe.msgprint("Please select Purchase Orders");
+                        return;
+                    }
 
-                        child.item_code = row.item_code;
-                        child.item_name = row.item_name;
-                        child.qty = row.qty;
-                        child.uom = row.uom;
+                    frappe.call({
+                        method: "gate_entry_app.api.get_multiple_po_items",
+                        args: { po_list: selections },
+                        callback: function(r) {
 
+                            frm.clear_table("items");
+
+                            (r.message || []).forEach(d => {
+
+                                let row = frm.add_child("items");
+
+                                row.item_code = d.item_code;
+                                row.item_name = d.item_name;
+                                row.qty = d.qty;
+                                row.uom = d.uom;
+                            });
+
+                            frm.refresh_field("items");
+
+                            // ✅ popup close
+                            d.dialog.hide();
+                        }
                     });
-
-                    frm.refresh_field("items");
                 }
-            }
-        });
+            });
 
+        });
     }
 });
